fa1fe186ed0efe7b695bbe0eacfec208a4a27a9d
Stepan Morgachev <stepan.morgachev@gmail.com>
2024-05-14 19:57:51 +0300

    feat: add exception handling

---
+++ src/main/java/ru/momo/monitoring/exceptions/user/ExceptionBody.java
@@ -0,0 +1,23 @@
+package ru.momo.monitoring.exceptions.user;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ExceptionBody {
+
+    String message;
+
+    Map<String, String> errors;
+
+    public ExceptionBody(String message) {
+        this.message = message;
+    }
+
+}
