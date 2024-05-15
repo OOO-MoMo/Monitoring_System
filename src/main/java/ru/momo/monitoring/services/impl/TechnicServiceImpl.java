abe86dc18ab692281a149d91b9fc5cd64367e751
Instmois <nikita-moiseev-2003@ya.ru>
2024-05-15 18:40:57 +0300

    add method getTechById

--- src/main/java/ru/momo/monitoring/services/impl/TechnicServiceImpl.java
+++ src/main/java/ru/momo/monitoring/services/impl/TechnicServiceImpl.java
@@ -7,15 +7,18 @@ import ru.momo.monitoring.store.dto.response.TechnicResponseDto;
 import ru.momo.monitoring.store.entities.Technic;
 import ru.momo.monitoring.store.repositories.TechnicRepository;
 
+import static ru.momo.monitoring.exceptions.user.ResourceNotFoundException.resourceNotFoundExceptionSupplier;
+
 @Service
 @RequiredArgsConstructor
 public class TechnicServiceImpl implements TechnicService {
+    private final TechnicRepository technicRepository;
     @Override
     public TechnicResponseDto getTechById(Long id) {
-        Technic technic = TechnicRepository
+        Technic technic = technicRepository
                 .findById(id)
                 .orElseThrow(
-
+                    resourceNotFoundExceptionSupplier("КУ ПРИВЕТ ГОЙДА!!!")
                 );
 
         return TechnicResponseDto.mapFromEntity(technic);
